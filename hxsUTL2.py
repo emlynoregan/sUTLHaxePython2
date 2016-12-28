f15cbebce6b1602c8125bae5e7ce79d50d00b008
Emlyn O'Regan <emlynoregan@gmail.com>
2016-12-28 14:38:28 +1030

    auto

--- hxsUTL2.py
+++ hxsUTL2.py
@@ -2704,3 +2704,65 @@ Math.PI = python_lib_Math.pi
 
 python_Boot.keywords = set(["and", "del", "from", "not", "with", "as", "elif", "global", "or", "yield", "assert", "else", "if", "pass", "None", "break", "except", "import", "raise", "True", "class", "exec", "in", "return", "False", "continue", "finally", "is", "try", "def", "for", "lambda", "while"])
 python_Boot.prefixLength = len("_hx_")
+
+
+#from util2 import Util2
+
+_keywords = set(["and", "del", "from", "not", "with", "as", "elif", "global", "or", "yield", "assert", "else", "if", "pass", "None", "break", "except", "import", "raise", "True", "class", "exec", "in", "return", "False", "continue", "finally", "is", "try", "def", "for", "lambda", "while"])
+
+class UtilReflect:
+    _hx_class_name = "UtilReflect"
+    _hx_statics = ["fields", "hasField", "field", "setField", "deleteField"]
+
+    @staticmethod
+    def calckey(key):
+        return "_hx_%s" % key if key in _keywords else key
+    
+    @staticmethod
+    def fields(obj):
+        return python_Boot.fields(obj)
+#     @staticmethod
+#     def fields(obj):
+#         retval = None
+#         
+#         if Util2.isObject(obj):
+#             retval = [UtilReflect.calckey(key) for key in obj.__dict__]
+# 
+#         return retval
+
+    @staticmethod
+    def hasField(obj,fieldname):
+        retval = False
+        
+        if Util2.isObject(obj):
+            key1 = UtilReflect.calckey(fieldname)
+            retval = hasattr(obj, key1)
+        return retval
+
+    @staticmethod
+    def field(obj,fieldname):
+        retval = None
+        
+        if Util2.isObject(obj):
+            key1 = UtilReflect.calckey(fieldname)
+            retval = getattr(obj, key1) if hasattr(obj, key1) else None
+        return retval
+
+    @staticmethod
+    def setField(obj,fieldname,value):
+        if Util2.isObject(obj):
+            key1 = UtilReflect.calckey(fieldname)
+            setattr(obj, key1, value)
+
+    @staticmethod
+    def deleteField(obj,fieldname):
+        retval = None
+        
+        if Util2.isObject(obj):
+            key1 = UtilReflect.calckey(fieldname)
+            if hasattr(obj, key1):
+                delattr(obj, key1)
+        return retval
+
+UtilReflect._hx_class = UtilReflect
+
