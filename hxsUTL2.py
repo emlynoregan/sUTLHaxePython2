5a507f4e2471cc574eaa7e0be227c59d05500575
Emlyn O'Regan <emlynoregan@gmail.com>
2016-12-28 14:58:36 +1030

    auto

--- hxsUTL2.py
+++ hxsUTL2.py
@@ -1602,7 +1602,7 @@ class SlowUtil3:
 
 
 
-class SlowUtilReflect:
+class UtilReflect:
 	_hx_class_name = "UtilReflect"
 	_hx_statics = ["fields", "hasField", "field", "setField", "deleteField"]
 
@@ -1626,7 +1626,7 @@ class SlowUtilReflect:
 	@staticmethod
 	def deleteField(obj,fieldname):
 		return Reflect.deleteField(obj,fieldname)
-
+UtilReflect._hx_class = UtilReflect
 
 
 class haxe_IMap:
@@ -2704,65 +2704,3 @@ Math.PI = python_lib_Math.pi
 
 python_Boot.keywords = set(["and", "del", "from", "not", "with", "as", "elif", "global", "or", "yield", "assert", "else", "if", "pass", "None", "break", "except", "import", "raise", "True", "class", "exec", "in", "return", "False", "continue", "finally", "is", "try", "def", "for", "lambda", "while"])
 python_Boot.prefixLength = len("_hx_")
-
-
-#from util2 import Util2
-
-# _keywords = set(["and", "del", "from", "not", "with", "as", "elif", "global", "or", "yield", "assert", "else", "if", "pass", "None", "break", "except", "import", "raise", "True", "class", "exec", "in", "return", "False", "continue", "finally", "is", "try", "def", "for", "lambda", "while"])
-
-class UtilReflect:
-    _hx_class_name = "UtilReflect"
-    _hx_statics = ["fields", "hasField", "field", "setField", "deleteField"]
-
-    @staticmethod
-    def calckey(key):
-        return "_hx_%s" % key if key in python_Boot.keywords else key
-    
-    @staticmethod
-    def fields(obj):
-        return python_Boot.fields(obj)
-#     @staticmethod
-#     def fields(obj):
-#         retval = None
-#          
-#         if Util2.isObject(obj):
-#             retval = [UtilReflect.calckey(key) for key in obj.__dict__]
-#  
-#         return retval
-
-    @staticmethod
-    def hasField(obj,fieldname):
-        retval = False
-        
-        if Util2.isObject(obj):
-            key1 = UtilReflect.calckey(fieldname)
-            retval = hasattr(obj, key1)
-        return retval
-
-    @staticmethod
-    def field(obj,fieldname):
-        retval = None
-        
-        if Util2.isObject(obj):
-            key1 = UtilReflect.calckey(fieldname)
-            retval = getattr(obj, key1) if hasattr(obj, key1) else None
-        return retval
-
-    @staticmethod
-    def setField(obj,fieldname,value):
-        if Util2.isObject(obj):
-            key1 = UtilReflect.calckey(fieldname)
-            setattr(obj, key1, value)
-
-    @staticmethod
-    def deleteField(obj,fieldname):
-        retval = None
-        
-        if Util2.isObject(obj):
-            key1 = UtilReflect.calckey(fieldname)
-            if hasattr(obj, key1):
-                delattr(obj, key1)
-        return retval
-
-UtilReflect._hx_class = UtilReflect
-
